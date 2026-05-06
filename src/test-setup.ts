4dd24cb48766101196ad2e7f7d66fe119b466713
Koen Van Geert <koen@Koens-MacBook-Air.local>
2026-05-06 10:27:20 +0200

    test: stub jsdom canvas getContext

--- src/test-setup.ts
+++ src/test-setup.ts
@@ -10,3 +10,21 @@ class MockWorker {
 }
 
 globalThis.Worker = MockWorker as unknown as typeof Worker
+
+if (typeof HTMLCanvasElement !== 'undefined') {
+  Object.defineProperty(HTMLCanvasElement.prototype, 'getContext', {
+    value: function getContext(this: HTMLCanvasElement, contextId: string) {
+      if (contextId !== '2d') return null
+
+      return {
+        canvas: this,
+        font: '',
+        measureText: (text: string) => ({ width: text.length * 7 }) as TextMetrics,
+        fillText: () => {},
+        clearRect: () => {},
+      } as unknown as CanvasRenderingContext2D
+    },
+    configurable: true,
+    writable: true,
+  })
+}
